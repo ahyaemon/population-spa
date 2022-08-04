a26a4257532f79adf7766dce2469b642acc2e0b7
ahyaemon <iamahyaemon@gmail.com>
2022-08-04 23:38:18 +0900

    とりあえずグラフっぽいのは出るようになった

--- src/components/graph/Bar.tsx
+++ src/components/graph/Bar.tsx
@@ -1,4 +1,4 @@
-import {Component, onMount} from "solid-js";
+import {Component} from "solid-js";
 import classes from "./Bar.module.css"
 
 type BarProps = {
@@ -11,18 +11,21 @@ type BarProps = {
 export const Bar: Component<BarProps> = (props) => {
     const { min, max, from, to } = props
     const n = (max - min) / 300
+    const isAscendant = to - from >= 0
 
+    const top = isAscendant ? to : from
+    const marginTop = (max - top) / n
     const height = Math.abs(to - from) / n
-    const lineBoxStyles = `margin-top: 100px; height: ${height}px`
+    const direction = isAscendant ? "bottom" : "top"
 
-    let a: HTMLDivElement
-
-    onMount(() => {
-        console.log(a)
-    })
+    const lineBoxStyles = `
+        margin-top: ${marginTop}px;
+        height: ${height}px;
+        background-image: linear-gradient(to right ${direction}, transparent 48%, black 48%, black 52%, transparent 52%);
+    `
 
     return (
-        <div class={classes.bar} ref={a}>
+        <div class={classes.bar}>
             <div class={classes.lineBox} style={lineBoxStyles}></div>
         </div>
     )
