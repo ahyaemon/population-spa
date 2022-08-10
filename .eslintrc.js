7c5c45dd0f1d511d6449987efb8374f02666fff1
ahyaemon <iamahyaemon@gmail.com>
2022-08-10 21:10:07 +0900

    re indent .eslintrc.js : 2 -> 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,48 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true,
-  },
-  extends: [
-    'airbnb-base',
-    'plugin:solid/typescript',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    '@typescript-eslint',
-    'solid',
-  ],
-  settings: {
-    // これを入れないと、Missing file extension "ts" で怒られる
-    'import/resolver': {
-      'node': {
-        'extensions': [
-          '.js',
-          '.jsx',
-          '.ts',
-          '.tsx',
-        ]
-      }
-    }
-  },
-  rules: {
-    semi: ['error', 'never'],
-    indent: ['error', 4],
-    // これを入れないと、Missing file extension "ts" で怒られる
-    'import/extensions': [
-      'error', 'always',
-      {
-        'js': 'never',
-        'jsx': 'never',
-        'ts': 'never',
-        'tsx': 'never',
-      }
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+    },
+    extends: [
+        'airbnb-base',
+        'plugin:solid/typescript',
+        'prettier',
     ],
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        '@typescript-eslint',
+        'solid',
+    ],
+    settings: {
+        // これを入れないと、Missing file extension "ts" で怒られる
+        'import/resolver': {
+            'node': {
+                'extensions': [
+                    '.js',
+                    '.jsx',
+                    '.ts',
+                    '.tsx',
+                ]
+            }
+        }
+    },
+    rules: {
+        semi: ['error', 'never'],
+        indent: ['error', 4],
+        // これを入れないと、Missing file extension "ts" で怒られる
+        'import/extensions': [
+            'error', 'always',
+            {
+                'js': 'never',
+                'jsx': 'never',
+                'ts': 'never',
+                'tsx': 'never',
+            }
+        ],
+    },
 };
